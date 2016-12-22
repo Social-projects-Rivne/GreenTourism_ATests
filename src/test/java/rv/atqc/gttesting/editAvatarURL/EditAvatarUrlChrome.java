462b06e842e000d724c6e2f4a5969232ef52f4e3
TyshchenkoRoma <tyshenko.roma@gmail.com>
2016-12-22 14:45:37 +0200

    RVAT-644_present_Change Button

---
+++ src/test/java/rv/atqc/gttesting/editAvatarURL/EditAvatarUrlChrome.java
@@ -0,0 +1,67 @@
+package rv.atqc.gttesting.editAvatarURL;
+
+import io.github.bonigarcia.wdm.ChromeDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+
+public class EditAvatarUrlChrome {
+    private WebDriver driver;
+    WebDriverWait wait;
+    private String USER_LOGIN = "17141@rambler.ru";
+    private String USER_PASSWORD = "12345678";
+
+    @BeforeMethod
+    public void before() {
+        ChromeDriverManager.getInstance().setup();
+        driver = new ChromeDriver();
+        driver.get("https://green-tourism.herokuapp.com/");
+        wait = new WebDriverWait(driver, 5);
+    }
+
+    @AfterMethod
+    public void afterMethod() {
+        driver.quit();
+    }
+
+    @Test
+    public void checkLogOutButtonTitleInChrome() {
+        clickOnLeftTopGreenButton();
+        insertLogin(USER_LOGIN);
+        insertPassword(USER_PASSWORD);
+        clickOnLoginButton();
+        waitForLoadProfilPage();
+        Assert.assertEquals(getChangeButtonText(), "Change");
+    }
+
+    private void clickOnLeftTopGreenButton() {
+        driver.findElement(By.cssSelector("html.ng-scope body header nav.navbar.navbar-default.navbar-fixed-top.navbar-style.ng-scope div.container.nav-padding div#navbar.collapse.navbar-collapse ul.nav.navbar-nav li.dropdown a.dropdown-toggle i.fa.fa-user.navtop")).click();
+    }
+
+    private void insertLogin(String login) {
+        driver.findElement(By.xpath("/html/body/header/nav/div/div[2]/ul[1]/li/ul/auth/div[2]/div/form/div[1]/input")).sendKeys(login);
+    }
+
+    private void insertPassword(String password) {
+        driver.findElement(By.xpath("/html/body/header/nav/div/div[2]/ul[1]/li/ul/auth/div[2]/div/form/div[2]/input")).sendKeys(password);
+    }
+
+    private void clickOnLoginButton() {
+        driver.findElement(By.cssSelector("html.ng-scope body header nav.navbar.navbar-default.navbar-fixed-top.navbar-style.ng-scope div.container.nav-padding div#navbar.collapse.navbar-collapse ul.nav.navbar-nav li.dropdown.open ul.dropdown-menu.dropdown-auth.nav-ul auth.ng-isolate-scope div.ng-scope div.container.registration.ng-scope form.ng-dirty.ng-valid-parse.ng-valid-required.ng-valid-email.ng-valid.ng-valid-pattern input.btn.btn-success.btn-block.submit-btn")).click();
+    }
+
+    private String getChangeButtonText() {
+        return driver.findElement(By.cssSelector("#main > div > user-profile > div > div > section.col-sm-3 > div.user-page__avatar > button > span:nth-child(1)")).getText();
+    }
+
+    private void waitForLoadProfilPage() {
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#main > div > user-profile > div > div > section.col-sm-3 > div.user-page__avatar > button > span:nth-child(1)")));
+    }
+}
