7be941ef64ca743928385ffe2fd391720c886e46
MaxKovalets <energmaxymys@gmail.com>
2016-12-23 12:50:34 +0300

    RVAT-593_PossibilityViewTheTitle

---
+++ src/test/java/rv/atqc/gttesting/possibility_view_main_page/PossibilityViewMainPageMozillaFirefox.java
@@ -0,0 +1,42 @@
+package rv.atqc.gttesting.possibility_view_main_page;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.FirefoxDriverManager;
+
+public class PossibilityViewMainPageMozillaFirefox {
+	
+	private WebDriver driver;
+	private WebDriverWait wait;
+	private final int MAX_WAIT_TIME=10;
+	
+	@BeforeClass
+	public void before(){
+		FirefoxDriverManager.getInstance().setup();
+		driver = new FirefoxDriver();
+		driver.get("https://green-tourism.herokuapp.com/");
+		wait = new WebDriverWait(driver,MAX_WAIT_TIME);
+	}
+
+	@AfterClass
+	public void afterMethod(){		
+		driver.quit();
+	}
+	
+	@Test
+	public void checkPossibilityViewTheTitle(){
+		WebElement titleElement = wait.until(ExpectedConditions.visibilityOfElementLocated 
+				                            (By.xpath("//h2[text()='Find. Plan. Travel']")));
+		Assert.assertTrue(titleElement.isDisplayed());
+	}
+}
