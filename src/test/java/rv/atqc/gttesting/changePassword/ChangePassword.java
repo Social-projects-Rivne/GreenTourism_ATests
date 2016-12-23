47dec84cb000172b004fee78919fe208875ebcff
Anton Romanukha <romanukhaav@gmail.com>
2016-12-23 03:46:09 +0200

    two_same_passwords_less_than_8_symbols

--- src/test/java/rv/atqc/gttesting/changePassword/ChangePassword.java
+++ src/test/java/rv/atqc/gttesting/changePassword/ChangePassword.java
@@ -1,7 +1,6 @@
 package rv.atqc.gttesting.changePassword;
 
 import io.github.bonigarcia.wdm.ChromeDriverManager;
-import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -16,9 +15,10 @@ public class ChangePassword {
 
 	private static String MAIL = "romanukhaav@gmail.com";
 	private static String PASS = "123456789";
+	private static String SHORT_PASS = "1234567";
 	private WebDriver driver;
 	private WebDriverWait wait;
-	private final int MAX_WAIT_TIME=10;
+	private final int MAX_WAIT_TIME = 10;
 	
 	@BeforeMethod
 	public void before() {
@@ -46,6 +46,16 @@ public class ChangePassword {
 		Assert.assertEquals(getTextFromFildNewPass(), "New password");
 	}
 	
+	
+	@Test
+	public void inputShortPasswords() {
+		clickOnChangePassButton();
+		inputPassFild1(SHORT_PASS);
+		inputPassFild2(SHORT_PASS);
+		Assert.assertEquals(getErrorText(), "Password should be longer than 8 characters");
+	}
+	
+	
 	private void clickOnLoginMenu() {
         driver.findElement(By.cssSelector("#navbar > ul:nth-child(1) > li")).click();
     }
@@ -68,6 +78,18 @@ public class ChangePassword {
 
 	 private String getTextFromFildNewPass() {
 	        return driver.findElement(By.xpath("//*[@id='main']/div/user-profile/div/div/section[2]/div[1]/div[3]/form/div[1]/input")).getAttribute("placeholder");
+	 }
+	 
+	 private void inputPassFild1(String pass) {
+	        driver.findElement(By.xpath("//*[@id='main']/div/user-profile/div/div/section[2]/div[1]/div[3]/form/div[1]/input")).sendKeys(pass);
+	 }
+	 
+	 private void inputPassFild2(String pass) {
+	        driver.findElement(By.xpath("//*[@id='main']/div/user-profile/div/div/section[2]/div[1]/div[3]/form/div[2]/input")).sendKeys(pass);
+	 }
+	 
+	 private String getErrorText() {
+	        return driver.findElement(By.xpath("//*[@id='main']/div/user-profile/div/div/section[2]/div[1]/div[3]/form/div[1]/div/p[2]")).getText();
 	 }
 	 	 
 }
