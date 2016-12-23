20bcad30c0112cf665e5e95130f14d2eb03a63f9
MaxKovalets <energmaxymys@gmail.com>
2016-12-23 13:22:23 +0300

    RVAT-615_PossibilityViewFacebookLink

--- src/test/java/rv/atqc/gttesting/possibility_view_main_page/PossibilityViewMainPageMozillaFirefox.java
+++ src/test/java/rv/atqc/gttesting/possibility_view_main_page/PossibilityViewMainPageMozillaFirefox.java
@@ -201,4 +201,11 @@ public class PossibilityViewMainPageMozillaFirefox {
 				                           (By.linkText("Google+")));
 		Assert.assertTrue(linkElement.isDisplayed());
 	}
+	
+	@Test
+	public void checkPossibilityViewFacebookLink(){
+		WebElement linkElement = wait.until(ExpectedConditions.visibilityOfElementLocated
+				                           (By.linkText("Facebook")));
+		Assert.assertTrue(linkElement.isDisplayed());
+	}
 }
