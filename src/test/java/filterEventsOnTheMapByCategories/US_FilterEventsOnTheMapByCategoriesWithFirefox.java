0557477d67c1ecfab52ab05d756a7db7cf5af044
Nick <nickwtwfu@gmail.com>
2016-12-28 16:00:57 +0200

    RVAT_725_726_VerifyTitleOfDropdownSubMenuEvents

--- src/test/java/filterEventsOnTheMapByCategories/US_FilterEventsOnTheMapByCategoriesWithFirefox.java
+++ src/test/java/filterEventsOnTheMapByCategories/US_FilterEventsOnTheMapByCategoriesWithFirefox.java
@@ -15,58 +15,68 @@ import org.testng.annotations.Test;
 /**
  * Created by Никита on 27.12.2016.
  */
-public class US_FilterEventsOnTheMapByCategoriesWithFirefox {
+    public class US_FilterEventsOnTheMapByCategoriesWithFirefox {
 
-    private WebDriver driver;
-    private WebDriverWait wait;
-    private final int MAX_WAIT_TIME=10;
+        private WebDriver driver;
+        private WebDriverWait wait;
+        private final int MAX_WAIT_TIME=10;
 
-    private WebElement categoriesDropdownList;
-    private WebElement eventsDropdownList;
+        private WebElement categoriesDropdownList;
+        private WebElement eventsDropdownList;
+        private WebElement eventsName;
 
-    private By categoriesLocator;
-    private By eventsLocator;
+        private By categoriesLocator;
+        private By eventsLocator;
+        private By eventsNameLocator;
 
-    private final String mapLink = "http://green-tourism.herokuapp.com/#!/places";
+        private final String mapLink = "http://green-tourism.herokuapp.com/#!/places";
 
-    @BeforeMethod
-    public void setUpDriverAndPreconditions() {
-        FirefoxDriverManager.getInstance().setup();
-        driver = new FirefoxDriver();
-        driver.manage().window().maximize();
-        wait = new WebDriverWait(driver,MAX_WAIT_TIME);
-        initializeLocators();
-        setUpPreconditions();
-    }
-
-    @AfterMethod
-    public void quitDriver() throws InterruptedException {
-        Thread.sleep(2000);
-        driver.quit();
-    }
-
-    @Test
-    public void verifyExistenceOfEventsDropdownList(){
-        Assert.assertTrue(exists(eventsDropdownList), "This element doesn't exist");
-    }
-
-    public void setUpPreconditions(){
-        driver.get(mapLink);
-        categoriesDropdownList = wait.until(ExpectedConditions.presenceOfElementLocated(categoriesLocator));
-        categoriesDropdownList.click();
-        eventsDropdownList = wait.until(ExpectedConditions.presenceOfElementLocated(eventsLocator));
-        eventsDropdownList.click();
-    }
-
-    public void initializeLocators(){
-        categoriesLocator = By.xpath("//*[@id=\"main\"]/div/place-list/div/div/div[1]/div[2]/div/div[3]");
-        eventsLocator = By.xpath("//*[@id=\"main\"]/div/place-list/div/div/div[1]/div[2]/div/div[3]/ul/li[3]");
-    }
-    public boolean exists(WebElement element){
-        if (element == null){
-            return false;
+        @BeforeMethod
+        public void setUpDriverAndPreconditions() {
+            FirefoxDriverManager.getInstance().setup();
+            driver = new FirefoxDriver();
+            driver.manage().window().maximize();
+            wait = new WebDriverWait(driver,MAX_WAIT_TIME);
+            initializeLocators();
+            setUpPreconditions();
         }
-        return true;
+
+        @AfterMethod
+        public void quitDriver() throws InterruptedException {
+            Thread.sleep(2000);
+            driver.quit();
+        }
+
+        @Test
+        public void verifyExistenceOfEventsDropdownList(){
+            Assert.assertTrue(exists(eventsDropdownList), "This element doesn't exist");
+        }
+
+        @Test
+        public void verifyTitleOfEventsDropdownList(){
+            Assert.assertTrue(eventsName.getText().equalsIgnoreCase("Events"));
+        }
+
+        public void initializeLocators(){
+            categoriesLocator = By.xpath("//*[@id=\"main\"]/div/place-list/div/div/div[1]/div[2]/div/div[3]");
+            eventsLocator = By.xpath("//*[@id=\"main\"]/div/place-list/div/div/div[1]/div[2]/div/div[3]/ul/li[3]");
+            eventsNameLocator = By.xpath("//*[@id=\"main\"]/div/place-list/div/div/div[1]/div[2]/div/div[3]/ul/li[3]/a");
+        }
+
+        public void setUpPreconditions(){
+            driver.get(mapLink);
+            categoriesDropdownList = wait.until(ExpectedConditions.presenceOfElementLocated(categoriesLocator));
+            categoriesDropdownList.click();
+            eventsDropdownList = wait.until(ExpectedConditions.presenceOfElementLocated(eventsLocator));
+            eventsDropdownList.click();
+            eventsName = wait.until(ExpectedConditions.presenceOfElementLocated(eventsNameLocator));
+        }
+
+        public boolean exists(WebElement element){
+            if (element == null){
+                return false;
+            }
+            return true;
+        }
     }
-}
 
