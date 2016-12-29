ff17193ba0ecdaf59b6808bd4741b39b51077e35
Nick <nickwtwfu@gmail.com>
2016-12-29 12:10:12 +0200

    Refactoring and change of method 'exists'.

--- src/test/java/filterEventsOnTheMapByCategories/US_FilterEventsOnTheMapByCategoriesWithChrome.java
+++ src/test/java/filterEventsOnTheMapByCategories/US_FilterEventsOnTheMapByCategoriesWithChrome.java
@@ -12,95 +12,86 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-/**
- * Created by Никита on 27.12.2016.
- */
-    public class US_FilterEventsOnTheMapByCategoriesWithChrome {
+public class US_FilterEventsOnTheMapByCategoriesWithChrome {
 
-        private WebDriver driver;
-        private WebDriverWait wait;
-        private final int MAX_WAIT_TIME=10;
+    private WebDriver driver;
+    private WebDriverWait wait;
+    private final int MAX_WAIT_TIME=10;
 
-        private WebElement categoriesDropdownList;
-        private WebElement eventsDropdownList;
-        private WebElement eventsName;
-        private WebElement gamePlaceFilter;
-        private WebElement festivalPlaceFilter;
-        private WebElement meetingPlaceFilter;
+    private WebElement categoriesDropdownList;
+    private WebElement eventsDropdownList;
+    private WebElement eventsName;
+    private WebElement gamePlaceFilter;
+    private WebElement festivalPlaceFilter;
+    private WebElement meetingPlaceFilter;
 
-        private By categoriesLocator;
-        private By eventsLocator;
-        private By eventsNameLocator;
-        private By gamePlaceFilterLocator;
-        private By festivalPlaceFilterLocator;
-        private By meetingPlaceFilterLocator;
+    private By categoriesLocator;
+    private By eventsLocator;
+    private By eventsNameLocator;
+    private By gamePlaceFilterLocator;
+    private By festivalPlaceFilterLocator;
+    private By meetingPlaceFilterLocator;
 
-        private final String mapLink = "http://green-tourism.herokuapp.com/#!/places";
+    private final String mapLink = "http://green-tourism.herokuapp.com/#!/places";
 
-        @BeforeMethod
-        public void setUpDriverAndPreconditions() {
-            ChromeDriverManager.getInstance().setup();
-            driver = new ChromeDriver();
-            driver.manage().window().maximize();
-            wait = new WebDriverWait(driver,MAX_WAIT_TIME);
-            initializeLocators();
-            setUpPreconditions();
-        }
-
-        @AfterMethod
-        public void quitDriver() throws InterruptedException {
-            Thread.sleep(2000);
-            driver.quit();
-        }
-
-        @Test
-        public void verifyExistenceOfEventsDropdownList(){
-            Assert.assertTrue(exists(eventsDropdownList), "This element doesn't exist");
-        }
-        @Test
-        public void verifyTitleOfEventsDropdownList(){
-            eventsName = wait.until(ExpectedConditions.presenceOfElementLocated(eventsNameLocator));
-            Assert.assertTrue(eventsName.getText().equalsIgnoreCase("Events"));
-        }
-        @Test
-        public void verifyExistenceOfPlaceFilterGame(){
-            gamePlaceFilter = wait.until(ExpectedConditions.presenceOfElementLocated(gamePlaceFilterLocator));
-            Assert.assertTrue(exists(gamePlaceFilter), "This element doesn't exist");
-        }
-        @Test
-        public void verifyExistenceOfPlaceFilterFestival(){
-            festivalPlaceFilter = wait.until(ExpectedConditions.presenceOfElementLocated(festivalPlaceFilterLocator));
-            Assert.assertTrue(exists(festivalPlaceFilter), "This element doesn't exist");
-        }
-        @Test
-        public void verifyExistenceOfPlaceFilterMeeting(){
-            meetingPlaceFilter = wait.until(ExpectedConditions.presenceOfElementLocated(meetingPlaceFilterLocator));
-            Assert.assertTrue(exists(meetingPlaceFilter), "This element doesn't exist");
-        }
-
-        public void initializeLocators(){
-            categoriesLocator = By.xpath("//*[@id=\"main\"]/div/place-list/div/div/div[1]/div[2]/div/div[3]");
-            eventsLocator = By.xpath("//*[@id=\"main\"]/div/place-list/div/div/div[1]/div[2]/div/div[3]/ul/li[3]");
-            eventsNameLocator = By.xpath("//*[@id=\"main\"]/div/place-list/div/div/div[1]/div[2]/div/div[3]/ul/li[3]/a");
-            gamePlaceFilterLocator = By.xpath("//*[@id=\"main\"]/div/place-list/div/div/div[1]/div[3]/div/div[5]/ul/li[1]");
-            festivalPlaceFilterLocator = By.xpath("//*[@id=\"main\"]/div/place-list/div/div/div[1]/div[2]/div/div[3]/ul/li[3]/ul/li[2]");
-            meetingPlaceFilterLocator = By.xpath("//*[@id=\"main\"]/div/place-list/div/div/div[1]/div[2]/div/div[3]/ul/li[3]/ul/li[3]");
-        }
-
-        public void setUpPreconditions(){
-            driver.get(mapLink);
-            categoriesDropdownList = wait.until(ExpectedConditions.presenceOfElementLocated(categoriesLocator));
-            categoriesDropdownList.click();
-            eventsDropdownList = wait.until(ExpectedConditions.presenceOfElementLocated(eventsLocator));
-            eventsDropdownList.click();
-        }
-
-        public boolean exists(WebElement element){
-            if (element == null){
-                return false;
-            }
-            return true;
-        }
+    @BeforeMethod
+    public void setUpDriverAndPreconditions() {
+        ChromeDriverManager.getInstance().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        wait = new WebDriverWait(driver,MAX_WAIT_TIME);
+        initializeLocators();
+        setUpPreconditions();
     }
+    @AfterMethod
+    public void quitDriver() throws InterruptedException {
+        driver.quit();
+    }
+    @Test
+    public void verifyExistenceOfEventsDropdownList(){
+        Assert.assertTrue(exists(eventsDropdownList), "This element doesn't exist");
+    }
+    @Test
+    public void verifyTitleOfEventsDropdownList(){
+        eventsName = wait.until(ExpectedConditions.presenceOfElementLocated(eventsNameLocator));
+        Assert.assertTrue(eventsName.getText().equalsIgnoreCase("Events"));
+    }
+    @Test
+    public void verifyExistenceOfPlaceFilterGame(){
+        gamePlaceFilter = wait.until(ExpectedConditions.presenceOfElementLocated(gamePlaceFilterLocator));
+        Assert.assertTrue(exists(gamePlaceFilter), "This element doesn't exist");
+    }
+    @Test
+    public void verifyExistenceOfPlaceFilterFestival(){
+        festivalPlaceFilter = wait.until(ExpectedConditions.presenceOfElementLocated(festivalPlaceFilterLocator));
+        Assert.assertTrue(exists(festivalPlaceFilter), "This element doesn't exist");
+    }
+    @Test
+    public void verifyExistenceOfPlaceFilterMeeting(){
+        meetingPlaceFilter = wait.until(ExpectedConditions.presenceOfElementLocated(meetingPlaceFilterLocator));
+        Assert.assertTrue(exists(meetingPlaceFilter), "This element doesn't exist");
+    }
+    public void initializeLocators(){
+        categoriesLocator = By.xpath("//*[@id=\"main\"]/div/place-list/div/div/div[1]/div[2]/div/div[3]");
+        eventsLocator = By.xpath("//*[@id=\"main\"]/div/place-list/div/div/div[1]/div[2]/div/div[3]/ul/li[3]");
+        eventsNameLocator = By.xpath("//*[@id=\"main\"]/div/place-list/div/div/div[1]/div[2]/div/div[3]/ul/li[3]/a");
+        gamePlaceFilterLocator = By.xpath("//*[@id=\"main\"]/div/place-list/div/div/div[1]/div[3]/div/div[5]/ul/li[1]");
+        festivalPlaceFilterLocator = By.xpath("//*[@id=\"main\"]/div/place-list/div/div/div[1]/div[2]/div/div[3]/ul/li[3]/ul/li[2]");
+        meetingPlaceFilterLocator = By.xpath("//*[@id=\"main\"]/div/place-list/div/div/div[1]/div[2]/div/div[3]/ul/li[3]/ul/li[3]");
+    }
+    public void setUpPreconditions(){
+        driver.get(mapLink);
+        categoriesDropdownList = wait.until(ExpectedConditions.presenceOfElementLocated(categoriesLocator));
+        categoriesDropdownList.click();
+        eventsDropdownList = wait.until(ExpectedConditions.presenceOfElementLocated(eventsLocator));
+        eventsDropdownList.click();
+    }
+    public boolean exists(WebElement element){
+        if (!element.isEnabled()){
+            return false;
+        }
+        return true;
+    }
+}
 
 
