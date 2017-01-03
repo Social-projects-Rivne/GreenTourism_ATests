2d14bbc9460dff46be3e922dd7dba20de97643c5
Maxim Kovalets <Energmaxymys@gmail.com>
2017-01-03 13:04:24 +0300

    RVAT-795_ExistenceStreetsItem

--- src/test/java/rv/atqc/gttesting/possibility_change_map_type/PossibilityChangeMapTypeMozillaPC.java
+++ src/test/java/rv/atqc/gttesting/possibility_change_map_type/PossibilityChangeMapTypeMozillaPC.java
@@ -80,5 +80,16 @@ public class PossibilityChangeMapTypeMozillaPC {
 				.elementToBeClickable(By.xpath("//button[@type='button' and contains" + "(., 'Map Type')]")));
 		Assert.assertTrue(mapTypeButton.isEnabled());
 	}
+	
+	@Test
+	public void checkExistenceStreetsItem() {
+		WebElement mapTypeButton = wait.until(ExpectedConditions
+				.elementToBeClickable(By.xpath("//button[@type='button' and contains" + "(., 'Map Type')]")));
+		mapTypeButton.click();
+		WebElement streetsItem = wait.until(ExpectedConditions
+				.presenceOfElementLocated(By.xpath("//div[@class='col-lg-5 col-md-5 hidden-sm"
+						+ " hidden-xs map-menu-buttons-wrapper']//a[@id='streets']")));
+	    Assert.assertTrue(streetsItem.isEnabled());
+	}
 
 }
