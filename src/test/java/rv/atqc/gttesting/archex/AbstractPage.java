3e845ee3fd6c76c665626824946753fab382de7a
TyshchenkoRoma <tyshenko.roma@gmail.com>
2017-01-19 15:37:50 +0200

    added 2 wait  methods

--- src/test/java/rv/atqc/gttesting/archex/AbstractPage.java
+++ src/test/java/rv/atqc/gttesting/archex/AbstractPage.java
@@ -1,45 +1,56 @@
 package rv.atqc.gttesting.archex;
 
 import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
-
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import rv.atqc.gttesting.archex.helpers.Acting;
 
 public abstract class AbstractPage<T extends AbstractPage<T>> {
 
-	protected WebDriver driver;
-	protected JavascriptExecutor jse;
+    protected WebDriver driver;
+    protected JavascriptExecutor jse;
+    protected WebDriverWait wait;
 
-	protected AbstractPage(WebDriver driver) {
-		this.driver = driver;
-		this.jse = (JavascriptExecutor) driver;
-		PageFactory.initElements(driver, this);
-	}
+    protected AbstractPage(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, 10);
+        this.jse = (JavascriptExecutor) driver;
+        PageFactory.initElements(driver, this);
+    }
 
-	public String getCurrentUrl() {
-		return driver.getCurrentUrl();
-	}
+    public String getCurrentUrl() {
+        return driver.getCurrentUrl();
+    }
 
-	public T disableFocus() {
-		return executeScript("document.activeElement.blur();");
-	}
+    public T disableFocus() {
+        return executeScript("document.activeElement.blur();");
+    }
 
-	public T executeScript(String script) {
-		jse.executeScript(script);
-		return (T) this;
-	}
-	
-	public T act(Acting acting){
-		acting.act(driver);
-		return (T) this;
-	}
-	
-	public T timeout(long time, TimeUnit timeUnit){
-		driver.manage().timeouts().implicitlyWait(time, timeUnit);
-		return (T) this;
-	}
-	
+    public T executeScript(String script) {
+        jse.executeScript(script);
+        return (T) this;
+    }
+
+    public T act(Acting acting){
+        acting.act(driver);
+        return (T) this;
+    }
+
+    public T waitForVisibilityOfElement(WebElement webEl) {
+        wait.until(ExpectedConditions.visibilityOf(webEl));
+        return (T) this;
+    }
+    public T waitForElementToBeClickable(WebElement element) {
+        wait.until(ExpectedConditions.elementToBeClickable(element));
+        return (T) this;
+    }
+
+    public T timeout(long time, TimeUnit timeUnit){
+        driver.manage().timeouts().implicitlyWait(time, timeUnit);
+        return (T) this;
+    }
 }
