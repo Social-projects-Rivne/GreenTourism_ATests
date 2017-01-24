b24b63561ea9f05906cf91044d6f21a4299861eb
TyshchenkoRoma <tyshenko.roma@gmail.com>
2017-01-24 16:43:56 +0200

    comm

---
+++ src/test/java/rv/atqc/gttesting/view_decription_page_of_map_pointer/ViewDecPageMapPointer_Chrome.java
@@ -0,0 +1,59 @@
+package rv.atqc.gttesting.view_decription_page_of_map_pointer;
+
+import io.github.bonigarcia.wdm.ChromeDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import rv.atqc.gttesting.archex.MapHeadPageForGuestUserPage;
+
+
+public class ViewDecPageMapPointer_Chrome {
+    private MapHeadPageForGuestUserPage page;
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void before() {
+        ChromeDriverManager.getInstance().setup();
+        driver = new ChromeDriver();
+        driver.get("https://green-tourism.herokuapp.com/#!/places");
+        page = new MapHeadPageForGuestUserPage(driver);
+    }
+
+    @AfterMethod
+    public void afterMethod() {
+        driver.quit();
+    }
+
+//    @Test
+//    public void checkPresentPlasesOnMap_Chrome() {
+//        Assert.assertTrue(page.getPlasesOnMapChrome().isDisplayed());
+//    }
+//
+//    @Test
+//    public void checkPresentButtonDetailsOnRaiseWindow_Chrome() {
+//        page.clickOnPlaceButton();
+//        Assert.assertTrue(page.getDetailsButton().isDisplayed());
+//    }
+
+    @Test
+    public void checkPresentButtonExitOnRaiseWindow_Chrome() {
+        page.clickOnPlaceButton();
+        Assert.assertTrue(page.getExitButtonOnRaiseWindow().isDisplayed());
+    }
+
+    @Test
+    public void checkPresentImageOnRaiseWindow_Chrome() {
+        page.clickOnPlaceButton();
+        Assert.assertTrue(page.getImageOnRaiseWindow().isDisplayed());
+    }
+
+
+    @Test
+    public void checkPresentsDatailsPage() {
+        page.clickOnPlaceButton();
+        Assert.assertTrue(page.getDetailsPage().isDisplayed());
+    }
+}
