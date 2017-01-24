a0787c43d9743fd865451a70a726f34df44203ce
TyshchenkoRoma <tyshenko.roma@gmail.com>
2017-01-24 12:49:58 +0200

    createt tests test

--- src/test/java/rv/atqc/gttesting/archex/MapHeadPageForGuestUserPage.java
+++ src/test/java/rv/atqc/gttesting/archex/MapHeadPageForGuestUserPage.java
@@ -1,11 +1,21 @@
 package rv.atqc.gttesting.archex;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
 
-public class MapHeadPageForGuestUserPage extends AbstractMapHeadPage<MapHeadPageForGuestUserPage>{
+public class MapHeadPageForGuestUserPage extends AbstractMapHeadPage<MapHeadPageForGuestUserPage> {
 
-	protected MapHeadPageForGuestUserPage(WebDriver driver) {
-		super(driver);
-	}
+    @FindBy(how = How.XPATH, using = "//*[@id=\"main\"]/div/place-list/div/div/div[1]/div[3]/div/div[2]/button")
+    private WebElement logIn;
+
+    public MapHeadPageForGuestUserPage(WebDriver driver) {
+        super(driver);
+    }
+    public void clickOn () {
+
+        logIn.click();
+    }
 
 }
