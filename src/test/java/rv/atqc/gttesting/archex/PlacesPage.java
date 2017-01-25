58416b51cd0d6a7cd787fc84d27d18eb64fa3b2a
Anton Romanukha <romanukhaav@gmail.com>
2017-01-25 02:05:45 +0200

    Anton/US-868_add_3_tests

--- src/test/java/rv/atqc/gttesting/archex/PlacesPage.java
+++ src/test/java/rv/atqc/gttesting/archex/PlacesPage.java
@@ -2,6 +2,7 @@ package rv.atqc.gttesting.archex;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -11,14 +12,15 @@ import org.openqa.selenium.support.How;
 
 public class PlacesPage extends AbstractPage<PlacesPage>{
 
-	private final int MAX_WAIT_TIME = 15;
-	private final String PLACES_PAGE_URL = "https://green-tourism.herokuapp.com/#!/places";
+	private final int MAX_WAIT_TIME = 10;
+
+	
 	private final String VISIBLE_ITEM_DETAILS_URL = "https://green-tourism.herokuapp.com/#!/places/57a4d84dca7a727c0ca59ba3";
 	private final String INVISIBLE_ITEM_DETAILS_URL = "https://green-tourism.herokuapp.com/#!/places/584e7ef0b61f280400d36ebd";
-	private final String  NO_ITEM_MESSAGE = "There are no such places and tracks, try else please";
-	
-	
+	private final String NO_ITEM_MESSAGE = "There are no such places and tracks, try else please";
+	public final String PLACES_PAGE_URL = "https://green-tourism.herokuapp.com/#!/places";
 
+	
 	@FindBy(how = How.XPATH, using = "//*[@id='map']/div[1]/div[2]/div[1]/img[1]")
 	private WebElement campPlace;
 	@FindBy(how = How.XPATH, using = "//*[@id='map']/div[1]/div[2]/div[1]/img[7]")
@@ -56,6 +58,8 @@ public class PlacesPage extends AbstractPage<PlacesPage>{
 	protected WebElement searchField;
 	@FindBy(how = How.XPATH, using = "//*[@id=\"search\"]/div/span[1]/button")
     private WebElement searchButton;
+    @FindBy(how = How.XPATH, using = "//*[@id=\"main\"]/div/place-list/div/div/div[2]/div[4]/a/i")
+    private WebElement closeResultsButton;
 	
 	@FindBy(how = How.XPATH, using = "//*[@id=\"search\"]/div/span[3]")
 	private WebElement failedRequestField;	
@@ -65,18 +69,15 @@ public class PlacesPage extends AbstractPage<PlacesPage>{
     private WebElement textSearchResults;    
     @FindBy(how = How.XPATH, using = "//*[@id=\"57a4d84dca7a727c0ca59ba3\"]/button/h3/a")
     private WebElement foundResultForVisibleItem;
-    @FindBy(how = How.XPATH, using = "//*[@id=\"584e7ef0b61f280400d36ebd\"]/button/h3/a")
+    @FindBy(how = How.XPATH, using = "//*[@id=\"57a4d84dca7a727c0ca59ba3\"]/button/h3/a")
+    private WebElement foundResultForSecondVisibleItem;
+    @FindBy(how = How.XPATH, using = "//*[@id=\"584e7ef0b61f280400d36ebd\"]/button/h3")
     private WebElement foundResultForInvisibleItem;
     @FindBy(how = How.XPATH, using = "//*[@id=\"map\"]/div[1]/div[2]/div[3]/img[1]")
-    private WebElement foundItem;
-    
-    @FindBy(how = How.XPATH, using = "//*[@id=\"map\"]/div[1]/div[2]/div[4]/div/div[1]/div/div/button/a")
-    private WebElement item1Details;
-    
-    @FindBy(how = How.XPATH, using = "//*[@id=\"main\"]/div/place-list/div/div/div[2]/div[4]/a/i")
-    private WebElement closeResultsButton;
-    
-  
+    private WebElement foundItem; 
+    @FindBy(how = How.XPATH, using = "//*[@id=\"map\"]/div[1]/div[2]/div[4]/div/div[1]/div/div/button")
+    private WebElement itemDetails;
+    	
 
 	public PlacesPage(WebDriver driver){
 		super(driver);
@@ -86,12 +87,27 @@ public class PlacesPage extends AbstractPage<PlacesPage>{
 		return NO_ITEM_MESSAGE;
 	}
 	
+	public String getInvisibleItemDetailsUrl(){
+		return INVISIBLE_ITEM_DETAILS_URL;
+	}
+
 	public String getVisibleItemDetailsUrl(){
 		return VISIBLE_ITEM_DETAILS_URL;
 	}
 	
-	public String getInisibleItemDetailsUrl(){
-		return INVISIBLE_ITEM_DETAILS_URL;
+	public WebElement getItemDetails(){
+		waitForVisibilityOfElement(itemDetails, MAX_WAIT_TIME);
+		return itemDetails;
+	}
+		
+	public WebElement getVisibleItemFoundResults(){
+		waitForVisibilityOfElement(foundResultForVisibleItem, MAX_WAIT_TIME);
+		return foundResultForVisibleItem;
+	}
+	
+	public WebElement getVisibleItemFoundSecondResult(){
+		waitForVisibilityOfElement(foundResultForSecondVisibleItem, MAX_WAIT_TIME);
+		return foundResultForSecondVisibleItem;
 	}
 	
 	public WebElement getInvisibleItemFoundResults(){
@@ -108,10 +124,16 @@ public class PlacesPage extends AbstractPage<PlacesPage>{
 		waitForVisibilityOfElement(textSearchResults, MAX_WAIT_TIME);
 		return textSearchResults;
 	}
-
+	
+	public PlacesPage timeout(long time, TimeUnit timeUnit) {
+		driver.manage().timeouts().implicitlyWait(time, timeUnit);
+		return this;
+	}
+	
 	public WebElement getFoundItem(){
 		waitForVisibilityOfElement(itemSearchResults, MAX_WAIT_TIME);
-		this.hoverElement(itemSearchResults);
+		hoverElement(itemSearchResults);
+		timeout(2,TimeUnit.SECONDS);
 		waitForElementToBeClickable(foundItem,MAX_WAIT_TIME);
 		return foundItem;
 	}
--- src/test/java/rv/atqc/gttesting/find_item_by_name/FindItemByNameChrome.java
+++ src/test/java/rv/atqc/gttesting/find_item_by_name/FindItemByNameChrome.java
@@ -18,6 +18,13 @@ public class FindItemByNameChrome {
 	private WebDriver driver;
 	private PlacesPage placesPage;
 	
+	private final String VISIBLE_ITEM_NAME = "ASdazxc";
+	private final String INVISIBLE_ITEM_NAME = "Hight Castle";
+	private final String FAILED_PLACE_NAME = "FAILED_PLACE";
+	private final String DOUBLE_ITEM_NAME = "sda";
+	private final String ERROR_ITEM_NAME = "1";
+	private final String SCRIPT = "<script>alert( 'Hello, World!' );</script>";
+	
 	@BeforeClass
 	public void beforeClass() {
 		ChromeDriverManager.getInstance().setup();
@@ -37,53 +44,62 @@ public class FindItemByNameChrome {
 	}
 	@AfterMethod
 	public void afterMethod(){
-		driver.get("https://green-tourism.herokuapp.com/#!/places");
+		driver.get(placesPage.PLACES_PAGE_URL);
 	}
 	@AfterClass
 	public void quitDriver(){
-	      driver.quit();
+		driver.quit();
 	}
-	
+
 	@Test
 	public void checkSearchResultsField(){
-		Assert.assertTrue(placesPage.lookForItem("ASdazxc").getTextSearchResults().isDisplayed());    
+		Assert.assertTrue(placesPage.lookForItem(VISIBLE_ITEM_NAME).getTextSearchResults().isDisplayed());    
 	}
-	
-  	@Test
+	@Test
 	public void checkSearchInptFieldSecurity(){
-  		Assert.assertEquals(placesPage.lookForItem("<script>alert( 'Hello, World!' );</script>"). getTextSearchResults().getText(), placesPage.getNoItemMessage());
+  		Assert.assertEquals(placesPage.lookForItem(SCRIPT). getTextSearchResults().getText(), placesPage.getNoItemMessage());
     }
-    
     @Test
     public void checkFailedRequestSearch(){
-		Assert.assertTrue(placesPage.lookForItem("1").getFailedRequestField().isDisplayed());
+		Assert.assertTrue(placesPage.lookForItem(ERROR_ITEM_NAME).getFailedRequestField().isDisplayed());
     }
-    
     @Test
     public void checkFailedPlaceSearch(){
-		Assert.assertEquals(placesPage.lookForItem("FAILED_PLACE"). getTextSearchResults().getText(), placesPage.getNoItemMessage());
+		Assert.assertEquals(placesPage.lookForItem(FAILED_PLACE_NAME). getTextSearchResults().getText(), placesPage.getNoItemMessage());
     }
-    
     @Test
     public void checkSearchResultsFromVisibleRegion(){
-		placesPage.lookForItem("ASdazxc").getFoundResults().click();
+		placesPage.lookForItem(VISIBLE_ITEM_NAME).getFoundResults().click();
     	Assert.assertEquals( driver.getCurrentUrl(), placesPage.getVisibleItemDetailsUrl());
     }
-    
     @Test
     public void checkSearchResultsFromInvisibleRegion(){
-		placesPage.lookForItem("Hight Castle").getInvisibleItemFoundResults().click();
-    	Assert.assertEquals( driver.getCurrentUrl(), placesPage.getInisibleItemDetailsUrl());
+		placesPage.lookForItem(INVISIBLE_ITEM_NAME).getInvisibleItemFoundResults().click();
+    	Assert.assertEquals( driver.getCurrentUrl(), placesPage.getInvisibleItemDetailsUrl());
 	}
-    
-    @Test
-    public void checkItemFromVisibleRegion(){
-		Assert.assertTrue(placesPage.lookForItem("ASdazxc").getFoundItem().isDisplayed());
-	}
-	
 	@Test
-    public void checkItemFromInisibleRegion(){
-		Assert.assertTrue(placesPage.lookForItem("Hight Castle").getFoundItem().isDisplayed());
+    public void checkItemFromVisibleRegion(){
+		Assert.assertTrue(placesPage.lookForItem(VISIBLE_ITEM_NAME).getFoundItem().isDisplayed());
 	}
- 
+	@Test
+    public void checkItemFromInvisibleRegion(){
+		Assert.assertTrue(placesPage.lookForItem(INVISIBLE_ITEM_NAME).getFoundItem().isDisplayed());
+	}
+	@Test
+    public void checkItemDetailsFromInvisibleRegion(){
+    	placesPage.lookForItem(INVISIBLE_ITEM_NAME).getFoundItem().click();
+    	Assert.assertTrue(placesPage.getItemDetails().isDisplayed());
+    }
+	@Test
+    public void checkItemDetailsUrlFromInvsibleRegion(){
+		placesPage.lookForItem(INVISIBLE_ITEM_NAME).getFoundItem().click();
+		placesPage.getItemDetails().click();
+    	Assert.assertEquals(driver.getCurrentUrl(),placesPage.getInvisibleItemDetailsUrl());
+    }
+	@Test
+    public void checkSearchResultsForTwoPlaces(){
+		placesPage.lookForItem(DOUBLE_ITEM_NAME);
+    	Assert.assertTrue(placesPage.getFoundResults().isDisplayed()&&placesPage.getVisibleItemFoundSecondResult().isDisplayed());
+    }
+	
 }
