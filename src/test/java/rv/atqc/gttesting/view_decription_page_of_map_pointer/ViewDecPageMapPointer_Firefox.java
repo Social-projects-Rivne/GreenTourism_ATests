fb2007f8305333399c9d372ac901f7a4b3737dbb
TyshchenkoRoma <tyshenko.roma@gmail.com>
2017-01-25 12:14:23 +0200

    commit

--- src/test/java/rv/atqc/gttesting/view_decription_page_of_map_pointer/ViewDecPageMapPointer_Firefox.java
+++ src/test/java/rv/atqc/gttesting/view_decription_page_of_map_pointer/ViewDecPageMapPointer_Firefox.java
@@ -1,23 +1,27 @@
 package rv.atqc.gttesting.view_decription_page_of_map_pointer;
 
 import io.github.bonigarcia.wdm.ChromeDriverManager;
+import io.github.bonigarcia.wdm.FirefoxDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import rv.atqc.gttesting.archex.MapHeadPageForGuestUserPage;
 
-
-public class ViewDecPageMapPointer_Chrome {
+/**
+ * Created by roman on 25.01.17.
+ */
+public class ViewDecPageMapPointer_Firefox {
     private MapHeadPageForGuestUserPage page;
     private WebDriver driver;
 
     @BeforeMethod
     public void before() {
-        ChromeDriverManager.getInstance().setup();
-        driver = new ChromeDriver();
+        FirefoxDriverManager.getInstance().setup();
+        driver = new FirefoxDriver();
         driver.get("https://green-tourism.herokuapp.com/#!/places");
         page = new MapHeadPageForGuestUserPage(driver);
     }
