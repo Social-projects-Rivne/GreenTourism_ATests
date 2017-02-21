fc88f9eb29432c0eb00ecb1a76947d36a951c13e
TyshchenkoRoma <tyshenko.roma@gmail.com>
2017-02-21 16:34:11 +0200

    delleted useless code

--- src/test/java/rv/atqc/gttesting/driverfactory/DriverFactory.java
+++ src/test/java/rv/atqc/gttesting/driverfactory/DriverFactory.java
@@ -4,15 +4,6 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 
 public enum DriverFactory {
     FIREFOX, CHROME;
-//    public static RemoteWebDriver init(String browser, int port){
-//        DriverFactory df = DriverFactory.valueOf(browser);
-//        switch (df){
-//            case FIREFOX: return new Firefox().init(port);
-//            case CHROME: return new Chrome().init(port);
-//            default: throw  new IllegalArgumentException();
-//        }
-//    }
-
     public static RemoteWebDriver init(DriverFactory df){
         switch (df){
             case FIREFOX: return new Firefox().init();
